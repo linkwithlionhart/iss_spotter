3a2f0ffd89a0b39c1fcaae556404c3dab1535b8e
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-13 16:55:07 -0700

    Modularize code and export

---
+++ printPassTimes.js
@@ -0,0 +1,10 @@
+const printPassTimes = (passTimes) => {
+  for (let pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+module.exports = { printPassTimes };
